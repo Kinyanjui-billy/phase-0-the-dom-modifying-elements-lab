053aeb30803ddff84fd5e197e18d3ead5d972641
Billy Kinyanjui <billy.kinyanjui@student.moringaschool.com>
2022-08-05 11:46:30 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.getElementById(`main`).remove();
+var newHeader = document.vreateElement(`h1`);
+newHeader.innerHtml = "LUKKAAA is the champion";
+// innertext = document.createTextNode("LUKAAA is the champion");
+//newheader.appendchild(innerText);
+document.body.appendChild(newHeader);
